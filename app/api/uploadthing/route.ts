c1b36373a0327ed0c47ce2d25ec9e7b949774120
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-09-19 18:48:51 +0530

    Refined image upload process by optimizing file validation and enhancing user feedback during uploads.

---
+++ app/api/uploadthing/route.ts
@@ -0,0 +1,10 @@
+// https://docs.uploadthing.com/getting-started/appdir
+
+import { createRouteHandler } from "uploadthing/next";
+ 
+import { ourFileRouter } from "./core";
+ 
+// Export routes for Next App Router
+export const { GET, POST } = createRouteHandler({
+  router: ourFileRouter,
+});
